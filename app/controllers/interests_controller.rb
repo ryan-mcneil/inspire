850604039c71d63bd458a7cbe88c38e73d6e3348
Ryan McNeil <ryan.mcneil288@gmail.com>
2019-02-23 21:40:20 -0700

    Simplify look up

--- app/controllers/interests_controller.rb
+++ app/controllers/interests_controller.rb
@@ -2,7 +2,7 @@ class InterestsController < ApplicationController
 
   def index
     if current_user
-      @interests = Interest.where(user_id: current_user.id)
+      @interests = Interest.where(user: current_user)
     else
       redirect_to '/login', notice: "You must log in first"
     end
