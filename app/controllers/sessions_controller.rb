6e72c7403405c883733cfe2e95e1e3e89d8bf6dc
Ryan McNeil <ryan.mcneil288@gmail.com>
2019-02-24 09:57:10 -0700

    fix bug
    incorrect email at login was not leading to a valid error/logic branch

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,13 +6,12 @@ class SessionsController < ApplicationController
   end
 
   def create
-
     user = User.find_by(email: params[:email])
 
     if user && user.authenticate(params[:password])
       session[:user_id] = user.id
       redirect_to root_path, notice: "Hi, #{user.username}!"
-    elsif user
+    else
       redirect_to login_path, notice: "Password does not match username. Please try again."
     end
   end
