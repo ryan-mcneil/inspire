f651f14e5786d20b744c3327835e73686e4aad34
Ryan McNeil <ryan.mcneil288@gmail.com>
2019-02-24 09:13:10 -0700

    Write failing test for delete interest

---
+++ spec/features/user_can_delete_interest_spec.rb
@@ -0,0 +1,28 @@
+require 'rails_helper'
+
+describe 'A user' do
+  it 'can delete an interest' do
+    user = User.create(email:'user@example.com', password:'password', username:'user')
+    i1 = Interest.create(name: 'a song', user: user)
+    i2 = Interest.create(name: 'a movie', user: user)
+
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+
+    visit '/interests'
+
+    within '.interests' do
+      expect(page).to have_content i1.name
+      expect(page).to have_content i2.name
+    end
+
+    within "#delete-interest-#{i1.id}" do
+      click_on "X"
+    end
+
+    within '.interests' do
+      expect(page).to_not have_content i1.name
+      expect(page).to have_content i2.name
+    end
+
+  end
+end
