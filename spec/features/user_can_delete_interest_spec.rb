a10e80a1a58f1593e0a6c196fd25647a98470dc6
Ryan McNeil <ryan.mcneil288@gmail.com>
2019-02-24 09:44:03 -0700

    Delete the aforementioned test
    no sad path to test

--- spec/features/user_can_delete_interest_spec.rb
+++ spec/features/user_can_delete_interest_spec.rb
@@ -25,26 +25,4 @@ describe 'A user' do
       expect(page).to have_content i2.name
     end
   end
-
-  it 'can delete an interest' do
-    user = User.create(email:'user@example.com', password:'password', username:'user')
-    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
-
-    visit '/interests'
-
-    within '.interests' do
-      expect(page).to have_content i1.name
-      expect(page).to have_content i2.name
-    end
-
-    within "#delete-interest-#{i1.id}" do
-      click_on "X"
-    end
-
-    expect(page).to have_content "#{i1.name.titleize} has been successfully deleted"
-    within '.interests' do
-      expect(page).to_not have_content i1.name
-      expect(page).to have_content i2.name
-    end
-  end
 end
