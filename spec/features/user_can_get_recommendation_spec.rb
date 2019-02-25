3585731bf9a59ff865af53697b38638c2b40c57d
Ryan McNeil <ryan.mcneil288@gmail.com>
2019-02-24 22:32:29 -0700

    Write failing Sad Path test

--- spec/features/user_can_get_recommendation_spec.rb
+++ spec/features/user_can_get_recommendation_spec.rb
@@ -1,11 +1,14 @@
 require 'rails_helper'
 
 describe 'A user' do
-  it 'gets a random recommendation' do
-    user = User.create(email:'user@example.com', password:'password', username:'user')
-    i1 = Interest.create(name: 'Red Hot Chili Peppers', user: user)
+  before :each do
+    @user = User.create(email:'user@example.com', password:'password', username:'user')
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@user)
+  end
 
-    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+  it 'gets a random recommendation' do
+    i1 = Interest.create(name: 'Red Hot Chili Peppers', user: @user)
+
 
     stub_request(:get, "https://tastedive.com/api/similar?q=Red Hot Chili Peppers&info=1").
       with(body: {k: ENV['TASTEDIVE_API_KEY']}).
@@ -29,6 +32,23 @@ describe 'A user' do
 
     within '#recommendation-teaser' do
       expect(page).to have_content("Foo Fighters is an American rock band, formed in Seattle")
+    end
+
+  end
+  it 'gets gives an error if a recommendation isnt found' do
+    i1 = Interest.create(name: 'Red Hot', user: @user)
+
+
+    stub_request(:get, "https://tastedive.com/api/similar?q=Red Hot&info=1").
+      with(body: {k: ENV['TASTEDIVE_API_KEY']}).
+      to_return(body: File.read('spec/fixtures/tastedive_bad.json'))
+
+    visit '/'
+
+    click_on "Inspire me!"
+
+    within '#recommendation' do
+      expect(page).to have_content("Try adding more interests for better recommendations!")
     end
 
   end
